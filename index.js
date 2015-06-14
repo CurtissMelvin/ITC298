2968df7a0f5ba0d213551bc2c33069d543c20ef6
Curtiss Melvin <CurtissMelvin@users.noreply.github.com>
2015-06-13 21:02:01 -0700

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+  //index.js
   var hapi = require("hapi");
   var server = new hapi.Server();
   server.connection({ port: 8000 });
@@ -9,27 +10,29 @@
     engines: {
       html: require("handlebars")
     },
-    path: "./views",
+    path: "./views/templates",
+    layoutPath: "layouts",
+    layout: "defaults",
     isCached: false
   });
 
-  //home page
-  server.route({
-    method: "GET",
-    path: "/", //localhost:8000/
-    handler: function(req, reply){
-      reply.view("index"); //index.html/
-    }
+  var List = require("./models/list");
+
+  var sql = require("./database");
+  sql.init(function() {
+    console.log("database ready");
+    var list = new Lists({
+      task: "Start server"
+    });
+    //console.log(list.toJSON());
+    list.create(function(err){
+      if(err) {
+        console.error(err);
+      }
+    });r
+
+    server.start();
   });
 
-  //static resources - CSS, images, etc.
-  //served from the build folder, since they're generated/copied via Grunt
-  server.route({
-    method: "GET",
-    path: "/assets/{param*}",
-    handler: {
-      directory: {
-        path: "build",
-      }
-    }
-  });
+  var routes = require("./routes");
+  server.route(routes);
